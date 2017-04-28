c0b72bf6c246279bb31eb30784073fa09502562a
shuvohabib <shuvohabib@gmail.com>
2017-04-29 03:38:04 +0600

    Add Project tooling setup and html structure for psd

---
+++ README.md
@@ -0,0 +1,12 @@
+# README #
+
+This is an assignment for Compucorp.
+
+### Setup ###
+
+Nothing Fancy! Click the *.html file to view in browser.
+
+### For Dev ###
+
+Install the node modules for running gulp by running `npm i` or `yarn`. 
+Run `gulp` in the terminal for running the site in browser.
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app',
+            index: "list.html"
+        }
+    })
+});
+
+gulp.task('default', ['serve','sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+
+});
